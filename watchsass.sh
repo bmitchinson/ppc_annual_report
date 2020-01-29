e0560453c97d17dc0a14caad51b73d3c11b6f90b
Ben Mitchinson <mitchinson.dev@gmail.com>
2020-01-29 14:03:05 -0600

    sass bang

--- watchsass.sh
+++ watchsass.sh
@@ -1,1 +1,2 @@
+#!/bin/bash
 sass --watch scss/global.scss:sites/default/files/annual-report/17-18/css/global.css & sass --watch scss/ar.scss:sites/default/files/annual-report/17-18/css/ar.css && fg
